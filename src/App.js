f4fbad125d9474d4aee5d8cae9c02f7d5c5bf749
wjd4204 <brian876373@gmail.com>
2025-02-19 11:28:18 +0900

    [Feature] 매장에 대한 화면 구현 (#27)
    * feat: Store 생성 페이지 추가
    
    * feat: Store 생성 페이지 추가
    
    * fix: 마이페이지에 회사 정보 추가
    
    * feat: 삭제 버튼 추가
    
    * feat: 누락된 페이지 import
    
    * feat: 누락된 페이지 import
    
    * feat: 주소 검색 UI 구현
    
    * feat: 주소 검색 UI 구현
    
    * feat: 주소 검색 결과를 입력 필드에 자동 생성
    
    * fix: 통합된 머지
    
    * feat: 매장 수정 페이지 구현

--- src/App.js
+++ src/App.js
@@ -14,52 +14,53 @@ import HeadquarterRecommendationPage from './pages/headquarter/HeadquarterRecomm
 import HeadquarterRegisterPage from './pages/headquarter/HeadquarterCreatePage';
 import ItemRegisterPage from './pages/headquarter/ItemRegisterPage';
 import HeadquarterDetailPage from './pages/headquarter/HeadquarterDetailPage';
+import CreateStore from '@/pages/store/CreateStore';
 import RecommendationStore from './pages/RecommendationStore';
 import NotificationPage from './pages/NotificationPage';
 import PromotionPage from './pages/PromotionPage';
+import UpdateStore from './pages/store/UpdateStore';
 import SSEManager from './components/SSEManager';
 
 const App = () => {
   return (
     <ChakraProvider>
-      <NotificationsProvider>
-        <BrowserRouter>
-          <SSEManager />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/change-store-id" element={<ChangeStoreId />} />
-            <Route path="/member-search" element={<MemberSearch />} />
-            <Route path="/delete-account" element={<DeleteAccount />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/nearest-store" element={<RecommendationStore />} />
-            <Route
-              path="/headquarter/store-recommendation"
-              element={<HeadquarterRecommendationPage />}
-            />
-            <Route
-              path="/headquarter/register"
-              element={<HeadquarterRegisterPage />}
-            />
-            <Route
-              path="/headquarter/mypage"
-              element={<HeadquarterDetailPage />}
-            />
-            <Route
-              path="/headquarter/item/register"
-              element={<ItemRegisterPage />}
-            />
-            <Route
-              path="/headquarter/store-recommendation"
-              element={<HeadquarterRecommendationPage />}
-            />
-            <Route path="/notifications" element={<NotificationPage />} />
-            <Route path="/promotions" element={<PromotionPage />} />
-            {/* 필요에 따라 추가 라우팅 */}
-          </Routes>
-        </BrowserRouter>
-      </NotificationsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+          <Route path="/change-store-id" element={<ChangeStoreId />} />
+          <Route path="/member-search" element={<MemberSearch />} />
+          <Route path="/delete-account" element={<DeleteAccount />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/nearest-store" element={<RecommendationStore />} />
+          <Route path="/createStore" element={<CreateStore />} />
+          <Route path="/updateStore" element={<UpdateStore />} />
+          <Route
+            path="/headquarter/store-recommendation"
+            element={<HeadquarterRecommendationPage />}
+          />
+          <Route
+            path="/headquarter/register"
+            element={<HeadquarterRegisterPage />}
+          />
+          <Route
+            path="/headquarter/mypage"
+            element={<HeadquarterDetailPage />}
+          />
+          <Route
+            path="/headquarter/item/register"
+            element={<ItemRegisterPage />}
+          />
+          <Route
+            path="/headquarter/store-recommendation"
+            element={<HeadquarterRecommendationPage />}
+          />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/promotions" element={<PromotionPage />} />
+          {/* 필요에 따라 추가 라우팅 */}
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 };
